7afc706b0eb3f71b292f957e27a8c291122386b9
antorad <66285305+antorad@users.noreply.github.com>
2025-07-18 16:51:26 -0400

    Create data_workflow.sh
    Added script to create a workflow to submit jobs to process reconstructed data files.

---
+++ data_workflow.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+MAIN_DIR=/work/clas12/rg-e/antorad/clas12-rge-analysis
+
+#Load clas12 modules
+module use /scigroup/cvmfs/hallb/clas12/sw/modulefiles
+module load clas12
+
+#Set ROOT location
+#HIPO location is set by clas12 module
+export ROOT=/u/scigroup/cvmfs/hallb/clas12/sw/almalinux9-gcc11/local/root/6.30.04/
+
+#Create workflow
+swif2 create test_data
+
+#Loop over run numbers from file adn add jobs to workflow
+while read -r RUN_NUMBER; do
+    COMMAND="./make_root_files_wf.sh -a -r $RUN_NUMBER"
+    #swif2 add-job test_data "cd $MAIN_DIR; $COMMAND"
+    echo 'testing command: $COMMAND'
+done < run_list.txt
